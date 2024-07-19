74147440894a413b86a81e37f1b8202ecf86e48f
weasel <paulbongers22@gmail.com>
2024-07-19 22:49:10 +0200

    x

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "Compile option master weasel:"
 echo "[1] Debug"
-echo "[2] Debug 64-bit"
-echo "[3] Regular compile"
-echo "[4] Optimized compile"
+echo "[2] Regular compile"
+echo "[3] Optimized compile"
 read -p "Enter your cunning choice: " choice
 
 compile_and_execute() {
@@ -16,10 +15,9 @@ compile_and_execute() {
 }
 
 case $choice in
-    1) gcc -o output_weasel_server weasel_server.c -g -Og -Wall -Wextra -Wpedantic -Og -g3 -fsanitize=address,undefined ;;
-    2) gcc -o output_weasel_server weasel_server.c -g -Og -Wall -Wextra -Wpedantic -Og -g3 -fsanitize=address,undefined -m64 ;;
-    3) gcc -o output_weasel_server weasel_server.c ;;
-    4) gcc -o output_weasel_server weasel_server.c -O3 ;;
+    1) gcc -g weasel_server.c -o output_weasel_server -Og -Wall -Wextra -Wpedantic -Og -g3 -fsanitize=address,undefined ;;
+    2) gcc -o output_weasel_server weasel_server.c ;;
+    3) gcc -o output_weasel_server weasel_server.c -O3 ;;
     *) echo "Invalid option" ;;
 esac
 
@@ -31,3 +29,7 @@ compile_and_execute
 # gcc $CommonFlags weasel_server.c -o weasel_server.x86_64 -g -Wl,-rpath,'$ORIGIN/x86_64'
 # # 32-bit build
 # gcc -m32 $CommonFlags weasel_server.c -o weasel_server.x86 -g -Wl,-rpath,'$ORIGIN/x86'
+# gcc -g weasel_server.c -o output_weasel_server 
+# # run gdb and regular exe:
+# gdb main
+# ./main
